3ac193d2d616d9b15710cbaebd1fb9d54c644acc
henritandre <rik.go.hss@gmail.com>
2026-05-04 10:52:44 -0300

    chore: removendo arquivo de credenciais do rastreamento

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vhazhoawfovzhmcwsyhf.supabase.co' //process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = 'sb_publishable_RHXO95k4jKULSV_33DGJnQ_17h7NhSj' //process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
